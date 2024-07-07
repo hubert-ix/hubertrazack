643f4b8c48cb943160e56c59dc7c91d4b3a6a551
Hubert Razack <hubert.razack@weareverse.com>
2024-07-07 19:03:10 -0400

    Added testimonials

--- src/lib/Testimonial.svelte
+++ src/lib/Testimonial.svelte
@@ -26,10 +26,25 @@
     text-align: center;
     margin-left: auto;
     margin-right: auto;
+    position: relative;
+  }
+
+  .wrap::before {
+    content: '';
+    background: url(/images/quote-orange.png) no-repeat;
+    background-size: 100%;
+    position: absolute;
+    width: 140px;
+    height: 140px;
+    left: -20px;
+    top: -60px;
+    opacity: 0.2;
   }
 
   .text {
     margin-bottom: 1rem;
+    font-family: "Playfair Display", serif;
+    font-size: 1.2rem;
   }
 
   .author-name {
