aead16d5bedbdfb87799602eac3d2f24852d99be
Hubert Razack <hubert.razack@weareverse.com>
2025-04-08 15:49:52 -0400

    Fixed desk page

--- src/routes/desk/+page.svelte
+++ src/routes/desk/+page.svelte
@@ -13,10 +13,10 @@
     <p>There are 3 6U rack spaces for your gear, and everything is built with strong solid wood. Its greatest strength is the cable management solution, which I had designed very specifically. There are openings for cables all throughout the desk, and a huge cable tray at the back. No cables in sight!</p>
     <p>And I shall add: it's proudly 100% Canadian-made :) I hope it finds a good home and will keep serving musicians!</p>
 
-      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk7.jpg?updatedAt=1744141315202&tr:w-500" /></p>
-      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk6.jpg?updatedAt=1744141315063&tr:w-500" /></p>
-      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk3.jpg?updatedAt=1744141315285&tr:w-500" /></p>
-      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk4.jpg?updatedAt=1744141315339&tr:w-500" /></p>
+      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk7.jpg?updatedAt=1744141315202&tr=w-500" /></p>
+      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk6.jpg?updatedAt=1744141315063&tr=w-500" /></p>
+      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk3.jpg?updatedAt=1744141315285&tr=w-500" /></p>
+      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk4.jpg?updatedAt=1744141315339&tr=w-500" /></p>
 
    </div>
   </section>
