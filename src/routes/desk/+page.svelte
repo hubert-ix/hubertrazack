ba5176491e72abe337663307bc65ba6855d50e08
Hubert Razack <hubert.razack@weareverse.com>
2025-04-08 15:46:27 -0400

    Added desk page

---
+++ src/routes/desk/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { fade } from "svelte/transition";
+</script>
+
+
+<div in:fade>
+  <section class="small first">
+    <div class="container">
+
+      <h2>Audio desk for sale in Toronto</h2>
+    <p>This audio production desk was custom made by MGear Design (Hamilton). It's modelled after the Platform Desk by Output, but with a few unique tweaks.</p>
+    <p>It has a keyboard tray large enough to hold a 88 key keyboard, with a cutout in the middle so that your legs fit easily while seating at the desk (I had it measured exactly). All with nice rounded corners!</p>
+    <p>There are 3 6U rack spaces for your gear, and everything is built with strong solid wood. Its greatest strength is the cable management solution, which I had designed very specifically. There are openings for cables all throughout the desk, and a huge cable tray at the back. No cables in sight!</p>
+    <p>And I shall add: it's proudly 100% Canadian-made :) I hope it finds a good home and will keep serving musicians!</p>
+
+      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk7.jpg?updatedAt=1744141315202&tr:w-500" /></p>
+      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk6.jpg?updatedAt=1744141315063&tr:w-500" /></p>
+      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk3.jpg?updatedAt=1744141315285&tr:w-500" /></p>
+      <p><img src="https://ik.imagekit.io/hubertrazack/examples/desk4.jpg?updatedAt=1744141315339&tr:w-500" /></p>
+
+   </div>
+  </section>
+</div>
+
+
